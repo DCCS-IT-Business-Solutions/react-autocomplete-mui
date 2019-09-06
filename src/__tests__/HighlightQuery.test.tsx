f0b2814c8dabae5c0ced54abb52d8adf876b9059
Resch Andreas (DCCS) <aresch@dccsintra.net>
2019-09-06 12:52:14 +0200

    implemented tests for HighlightQuery

--- src/__tests__/HighlightQuery.test.tsx
+++ src/__tests__/HighlightQuery.test.tsx
@@ -17,11 +17,38 @@ it("should render with text being null", () => {
   expect(sut).toMatchSnapshot();
 });
 
-it("should highlight 'test'", () => {
-  const sut = shallow(HighlightQuery("this is a test string", "test"));
+it("should render with custom style", () => {
+  const sut = render(HighlightQuery(null, "test", { backgroundColor: "red" }));
+  expect(sut).toMatchSnapshot();
+});
 
-  const highlightedSpan = sut.find("span > span").at(0);
-  // tslint:disable-next-line: no-console
-  expect(highlightedSpan).not.toBeNull();
-  // expect(highlightedSpan).toHaveProperty("style.fontWeight");
+it("should highlight 'test'", () => {
+  const sut = shallow(HighlightQuery("this is a test string", "test")).setProps(
+    {}
+  );
+  const highlightedSpan = sut.find("span > span").get(0);
+  const style = highlightedSpan.props.style.fontWeight;
+  expect(style).toBe("bold");
+
+  const text = sut
+    .find("span > span")
+    .at(0)
+    .text();
+  expect(text).toBe("test");
+});
+
+it("should not highlight anything", () => {
+  const sut = shallow(HighlightQuery("this is a test string", "")).setProps({});
+
+  expect(sut.children("span").length).toBe(0);
+});
+
+it("should highlight 'test' with custom style", () => {
+  const sut = shallow(
+    HighlightQuery("this is a test string", "test", { backgroundColor: "red" })
+  ).setProps({});
+
+  const highlightedSpan = sut.find("span > span").get(0);
+  const style = highlightedSpan.props.style.backgroundColor;
+  expect(style).toBe("red");
 });
